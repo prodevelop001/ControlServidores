ff11ed5df7b38ffc6cdb9599d0772d8035cdf255
Mauro A. <ma_10fig@hotmail.com>
2015-10-07 18:15:53 -0500

    Editando ConceptoEstatus.aspx

--- ControlServidores.Web/Scripts/general.js
+++ ControlServidores.Web/Scripts/general.js
@@ -10,3 +10,15 @@ var i;
 for (i = 0 ; i < y.length; i++) {
     y[i].className = y.className + " icon-cross";
 }
+//-----------------------------Concepto Estatus---------------------------------------
+var x2 = document.querySelectorAll("#cuerpoPpal_gdvConceptos tbody tr td:nth-child(4) a");
+var i2;
+for (i2 = 0 ; i2 < x2.length; i2++) {
+    x2[i2].className = x2.className + " icon-pencil";
+}
+
+var y2 = document.querySelectorAll("#cuerpoPpal_gdvConceptos tbody tr td:nth-child(5) a");
+var i2;
+for (i2 = 0 ; 2 < y2.length; i2++) {
+    y2[i2].className = y2.className + " icon-cross";
+}
--- ControlServidores.Web/prueba.Master.designer.cs
+++ ControlServidores.Web/prueba.Master.designer.cs
@@ -67,12 +67,12 @@ namespace ControlServidores.Web {
         protected global::System.Web.UI.WebControls.LinkButton cerrarSesion;
         
         /// <summary>
-        /// ContentPlaceHolder1 control.
+        /// cuerpoPpal control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder cuerpoPpal;
     }
 }
