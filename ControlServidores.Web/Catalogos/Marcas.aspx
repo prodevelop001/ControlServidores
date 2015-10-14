dcea35c79eb7cc5fedf7d2b9b5c060037c60a00d
Mauro A. <ma_10fig@hotmail.com>
2015-10-14 14:34:05 -0500

    Catalogo Marcas

---
+++ ControlServidores.Web/Catalogos/Marcas.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Sitio.Master" AutoEventWireup="true" CodeBehind="Marcas.aspx.cs" Inherits="ControlServidores.Web.Catalogos.Marcas" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="cabecera" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cuerpoPpal" runat="server">
+<div  class="principal">
+    <div id="numCols" class="hide">5</div>
+    <div class="ttlPrincipal">
+        <h1>Marcas</h1>
+    </div>
+    <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>
+            <div class="addNuevo">
+                <asp:HiddenField ID="hdfEstado" runat="server"></asp:HiddenField>
+                <asp:Button ID="btnNuevo" CssClass="btnNuevo" runat="server" Text="Nuevo" OnClick="btnNuevo_Click"/>
+            </div>
+            <asp:Panel ID="pnlFormulario" CssClass="miFormulario" runat="server">
+                <div class="formCampos">
+                    <asp:HiddenField ID="lblIdMarca" runat="server" />
+                    <div class="grpInput">
+                        <label>Nombre de la Marca</label>
+                        <asp:TextBox ID="txtMarca" runat="server"></asp:TextBox>
+                        <asp:RequiredFieldValidator ID="rfvMarca" runat="server" ErrorMessage="*Concepto requerido." ControlToValidate="txtMarca" ForeColor="Red" ValidationGroup="Marcas"></asp:RequiredFieldValidator>
+                    </div>
+                </div>
+                <div class="formBotones">
+                    <asp:Button ID="btnGuardar" CssClass="btnGuardar" runat="server" Text="Guardar" OnClick="btnGuardar_Click" ValidationGroup="SistemasOperativos"></asp:Button>
+                    <asp:Button ID="btnCancelar" CssClass="btnCancelar" runat="server" Text="Cancelar" OnClick="btnCancelar_Click"></asp:Button>
+                </div>
+            </asp:Panel>
+            <asp:Panel ID="pnlMarca" runat="server">
+                <asp:GridView ID="gdvMarcas" runat="server" CssClass="miTabla5" AutoGenerateColumns="False" OnRowDataBound="gdvMarcas_RowDataBound" OnRowDeleting="gdvMarcas_RowDeleting" OnSelectedIndexChanged="gdvMarcas_SelectedIndexChanged">
+                    <Columns>
+                        <asp:TemplateField HeaderText="#">
+                            <ItemTemplate>
+                                <%# Container.DisplayIndex + 1%>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:BoundField DataField="IdMarca" HeaderText="IdMarca">
+                            <HeaderStyle CssClass="hide" />
+                            <ItemStyle CssClass="hide" />
+                        </asp:BoundField>
+                        <asp:BoundField DataField="NombreMarca" HeaderText="Nombre de la Marca" />
+                        <asp:CommandField SelectText="Seleccionar" ShowSelectButton="True" />
+                        <asp:CommandField DeleteText="Eliminar" ShowDeleteButton="True" />
+                    </Columns>
+                </asp:GridView>
+            </asp:Panel>
+            <div>
+                <asp:Label ID="lblStatus" runat="server" Text=""></asp:Label>
+            </div>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+</div> <%--Fin de Div Principal--%>
+</asp:Content>
---
+++ ControlServidores.Web/Catalogos/Marcas.aspx.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ControlServidores.Web.Catalogos
+{
+    public partial class Marcas : System.Web.UI.Page
+    {
+        Entidades.RolUsuario permisos = new Entidades.RolUsuario();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Este ID debe coincidir con el Menú registrado en la BD
+            int IdPagina = 13;
+            if (Negocio.Seguridad.Seguridad.AccesoPagina(IdPagina) == true)
+            {
+                if (!IsPostBack)
+                {
+                    permisos = Negocio.Seguridad.Seguridad.verificarPermisos();
+                    if (permisos.R == true)
+                    {
+                        pnlMarca.Visible = true;
+                        pnlFormulario.Visible = false;
+                        llenarGdvMarcas();
+                    }
+                    btnNuevo.Enabled = permisos.C;
+                }
+            }
+            else
+            {
+                Response.Redirect("~/errorAcceso.aspx");
+            }
+        }//Fin de Void
+
+        private void llenarGdvMarcas()
+        {
+
+            permisos = Negocio.Seguridad.Seguridad.verificarPermisos();
+
+            gdvMarcas.DataSource = Negocio.Catalogos.MarcaServidor.obtenerMarcaServidor(new Entidades.MarcaServidor());
+            gdvMarcas.DataBind();
+        }//Fin de Llenar Gridview SOs
+
+        protected void btnNuevo_Click(object sender, EventArgs e)
+        {
+            permisos = Negocio.Seguridad.Seguridad.verificarPermisos();
+            hdfEstado.Value = "1";
+            lblStatus.Text = string.Empty;
+            btnGuardar.Text = "Guardar";
+            btnGuardar.Enabled = permisos.C;
+            pnlMarca.Visible = false;
+            pnlFormulario.Visible = true;
+            lblIdMarca.Value = string.Empty;
+            //lblIdSistemaOperativo.Attributes["style"] = "display: none;";
+            txtMarca.Text = string.Empty;
+        }//Fin de Boton Nuevo
+
+        protected void btnCancelar_Click(object sender, EventArgs e)
+        {
+            hdfEstado.Value = "0";
+            btnNuevo.Visible = true;
+            btnNuevo.Text = "Nuevo";
+            pnlMarca.Visible = true;
+            pnlFormulario.Visible = false;
+        }//Fin de Boton Cancelar
+
+
+        protected void gdvMarcas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            permisos = Negocio.Seguridad.Seguridad.verificarPermisos();
+            hdfEstado.Value = "2";
+            lblStatus.Text = string.Empty;
+            btnNuevo.Visible = false;
+            btnGuardar.Text = "Actualizar";
+            btnGuardar.Enabled = permisos.U;
+            pnlMarca.Visible = false;
+            pnlFormulario.Visible = true;
+            lblIdMarca.Value = gdvMarcas.SelectedRow.Cells[1].Text;
+
+            txtMarca.Text = gdvMarcas.SelectedRow.Cells[2].Text;
+        }//Fin de Seleccionar en Gridview
+
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+            permisos = Negocio.Seguridad.Seguridad.verificarPermisos();
+            lblStatus.Text = string.Empty;
+            Entidades.Logica.Ejecucion resultado = new Entidades.Logica.Ejecucion();
+            if (hdfEstado.Value == "1" && permisos.C == true)
+            {
+                resultado = Negocio.Catalogos.MarcaServidor.Nuevo(new Entidades.MarcaServidor()
+                {
+                    NombreMarca = txtMarca.Text
+                });
+            }
+            else if (hdfEstado.Value == "2" && permisos.U == true)
+            {
+                resultado = Negocio.Catalogos.MarcaServidor.Actualizar(new Entidades.MarcaServidor()
+                {
+                    IdMarca = Convert.ToInt32(lblIdMarca.Value),
+                    NombreMarca = txtMarca.Text
+                });
+            }
+            else
+            {
+                lblStatus.Text = "No tienes privilegios para realizar esta acción.";
+            }
+
+            resultado.errores.ForEach(delegate (Entidades.Logica.Error error)
+            {
+                lblStatus.Text += error.descripcionCorta + "<br/>";
+            });
+
+            lblStatus.ForeColor = System.Drawing.Color.Red;
+            if (resultado.resultado == true)
+            {
+                lblStatus.ForeColor = System.Drawing.Color.Green;
+                hdfEstado.Value = "0";
+                btnNuevo.Visible = true;
+                btnNuevo.Text = "Nuevo";
+                pnlMarca.Visible = true;
+                pnlFormulario.Visible = false;
+                llenarGdvMarcas();
+            }
+        }//Fin de boton Guardar
+
+        protected void gdvMarcas_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                foreach (DataControlFieldCell cell in e.Row.Cells)
+                {
+                    foreach (Control control in cell.Controls)
+                    {
+                        //LinkButton button = (LinkButton)control;
+                        LinkButton button = control as LinkButton;
+                        if (button != null && button.Text == "Eliminar")
+                        {
+                            button.Enabled = permisos.D;
+                            if (button.Enabled)
+                                button.OnClientClick = "return checkMe()";
+                        }
+                    }
+                }
+            }
+        }//Fin de Row Data Bound
+
+        protected void gdvMarcas_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            lblStatus.Text = string.Empty;
+            int IdMarca = Convert.ToInt32(gdvMarcas.Rows[e.RowIndex].Cells[1].Text);
+            Entidades.Logica.Ejecucion resultado = new Entidades.Logica.Ejecucion();
+            resultado = Negocio.Catalogos.MarcaServidor.Eliminar(new Entidades.MarcaServidor()
+            {
+                IdMarca = IdMarca
+            });
+
+            resultado.errores.ForEach(delegate (Entidades.Logica.Error error)
+            {
+                lblStatus.Text += error.descripcionCorta + "<br/>";
+            });
+
+            lblStatus.ForeColor = System.Drawing.Color.Red;
+            if (resultado.resultado == true)
+            {
+                lblStatus.ForeColor = System.Drawing.Color.Green;
+                llenarGdvMarcas();
+            }
+        }//Fin eliminar Fila
+    }
+}
