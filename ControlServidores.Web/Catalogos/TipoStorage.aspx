322fc4e791213dcd3923220e366acb0ec5677209
Mauro A. <ma_10fig@hotmail.com>
2015-10-15 12:12:03 -0500

    Catalogo Tipo de Storage

---
+++ ControlServidores.Web/Catalogos/TipoStorage.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Sitio.Master" AutoEventWireup="true" CodeBehind="TipoStorage.aspx.cs" Inherits="ControlServidores.Web.Catalogos.TipoStorage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="cabecera" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cuerpoPpal" runat="server">
+<div  class="principal">
+    <div id="numCols" class="hide">5</div>
+    <div class="ttlPrincipal">
+        <h1>Tipo de Storage</h1>
+    </div>
+    <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>
+            <div class="addNuevo">
+                    <asp:HiddenField ID="hdfEstado" runat="server"></asp:HiddenField>
+                    <asp:Button ID="btnNuevo" CssClass="btnNuevo" runat="server" Text="Nuevo" OnClick="btnNuevo_Click"/>
+            </div>
+            <asp:Panel ID="pnlFormulario" CssClass="miFormulario" runat="server">
+                <div class="formCampos">
+                    <asp:HiddenField ID="lblIdTipoStorage" runat="server" />
+                    <div class="grpInput">
+                        <label>Tipo de Storage</label>
+                        <asp:TextBox ID="txtTipoStorage" runat="server"></asp:TextBox>
+                        <asp:RequiredFieldValidator ID="rfvTipoStorage" runat="server" ErrorMessage="*Concepto requerido." ControlToValidate="txtTipoStorage" ForeColor="Red" ValidationGroup="TiposStorage"></asp:RequiredFieldValidator>
+                    </div>
+                </div>
+                <div class="formBotones">
+                    <asp:Button ID="btnGuardar" CssClass="btnGuardar" runat="server" Text="Guardar" OnClick="btnGuardar_Click" ValidationGroup="TiposStorage"></asp:Button>
+                    <asp:Button ID="btnCancelar" CssClass="btnCancelar" runat="server" Text="Cancelar" OnClick="btnCancelar_Click"></asp:Button>
+                </div>
+            </asp:Panel>
+            <asp:Panel ID="pnlTipoStorage" runat="server">
+                <asp:GridView ID="gdvTiposStorage" runat="server" CssClass="miTabla5" AutoGenerateColumns="False" OnRowDataBound="gdvTiposStorage_RowDataBound" OnRowDeleting="gdvTiposStorage_RowDeleting" OnSelectedIndexChanged="gdvTiposStorage_SelectedIndexChanged">
+                    <Columns>
+                        <asp:TemplateField HeaderText="#">
+                            <ItemTemplate>
+                                <%# Container.DisplayIndex + 1%>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:BoundField DataField="IdTipoStorage" HeaderText="IdTipoStorage">
+                            <HeaderStyle CssClass="hide" />
+                            <ItemStyle CssClass="hide" />
+                        </asp:BoundField>
+                        <asp:BoundField DataField="Tipo" HeaderText="Tipo de Storage" />
+                        <asp:CommandField SelectText=" Seleccionar" ShowSelectButton="True" />
+                        <asp:CommandField DeleteText=" Eliminar" ShowDeleteButton="True" />
+                    </Columns>
+                </asp:GridView>
+            </asp:Panel>
+            <div>
+                <asp:Label ID="lblStatus" runat="server" Text=""></asp:Label>
+            </div>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+</div> <%--Fin de Div Principal--%>
+</asp:Content>
---
+++ ControlServidores.Web/Catalogos/TipoStorage.aspx.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ControlServidores.Web.Catalogos
+{
+    public partial class TipoStorage : System.Web.UI.Page
+    {
+        Entidades.RolUsuario permisos = new Entidades.RolUsuario();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Este ID debe coincidir con el Menú registrado en la BD
+            int IdPagina = 19;
+            if (Negocio.Seguridad.Seguridad.AccesoPagina(IdPagina) == true)
+            {
+                if (!IsPostBack)
+                {
+                    permisos = Negocio.Seguridad.Seguridad.verificarPermisos();
+                    if (permisos.R == true)
+                    {
+                        pnlTipoStorage.Visible = true;
+                        pnlFormulario.Visible = false;
+                        llenarGdvTiposStorage();
+                    }
+                    btnNuevo.Enabled = permisos.C;
+                }
+            }
+            else
+            {
+                Response.Redirect("~/errorAcceso.aspx");
+            }
+        }//Fin de Void
+
+        private void llenarGdvTiposStorage()
+        {
+
+            permisos = Negocio.Seguridad.Seguridad.verificarPermisos();
+
+            gdvTiposStorage.DataSource = Negocio.Catalogos.TipoStorage.Obtener(new Entidades.TipoStorage());
+            gdvTiposStorage.DataBind();
+        }//Fin de Llenar Gridview SOs
+
+        protected void btnNuevo_Click(object sender, EventArgs e)
+        {
+            permisos = Negocio.Seguridad.Seguridad.verificarPermisos();
+            hdfEstado.Value = "1";
+            lblStatus.Text = string.Empty;
+            btnGuardar.Text = "Guardar";
+            btnGuardar.Enabled = permisos.C;
+            pnlTipoStorage.Visible = false;
+            pnlFormulario.Visible = true;
+            lblIdTipoStorage.Value = string.Empty;
+            //lblIdSistemaOperativo.Attributes["style"] = "display: none;";
+            txtTipoStorage.Text = string.Empty;
+        }//Fin de Boton Nuevo
+
+        protected void btnCancelar_Click(object sender, EventArgs e)
+        {
+            hdfEstado.Value = "0";
+            btnNuevo.Visible = true;
+            btnNuevo.Text = "Nuevo";
+            pnlTipoStorage.Visible = true;
+            pnlFormulario.Visible = false;
+        }//Fin de Boton Cancelar
+
+
+        protected void gdvTiposStorage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            permisos = Negocio.Seguridad.Seguridad.verificarPermisos();
+            hdfEstado.Value = "2";
+            lblStatus.Text = string.Empty;
+            btnNuevo.Visible = false;
+            btnGuardar.Text = "Actualizar";
+            btnGuardar.Enabled = permisos.U;
+            pnlTipoStorage.Visible = false;
+            pnlFormulario.Visible = true;
+            lblIdTipoStorage.Value = gdvTiposStorage.SelectedRow.Cells[1].Text;
+
+            txtTipoStorage.Text = gdvTiposStorage.SelectedRow.Cells[2].Text;
+        }//Fin de Seleccionar en Gridview
+
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+            permisos = Negocio.Seguridad.Seguridad.verificarPermisos();
+            lblStatus.Text = string.Empty;
+            Entidades.Logica.Ejecucion resultado = new Entidades.Logica.Ejecucion();
+            if (hdfEstado.Value == "1" && permisos.C == true)
+            {
+                resultado = Negocio.Catalogos.TipoStorage.Nuevo(new Entidades.TipoStorage()
+                {
+                    Tipo = txtTipoStorage.Text
+                });
+            }
+            else if (hdfEstado.Value == "2" && permisos.U == true)
+            {
+                resultado = Negocio.Catalogos.TipoStorage.Actualizar(new Entidades.TipoStorage()
+                {
+                    IdTipoStorage = Convert.ToInt32(lblIdTipoStorage.Value),
+                    Tipo = txtTipoStorage.Text
+                });
+            }
+            else
+            {
+                lblStatus.Text = "No tienes privilegios para realizar esta acción.";
+            }
+
+            resultado.errores.ForEach(delegate (Entidades.Logica.Error error)
+            {
+                lblStatus.Text += error.descripcionCorta + "<br/>";
+            });
+
+            lblStatus.ForeColor = System.Drawing.Color.Red;
+            if (resultado.resultado == true)
+            {
+                lblStatus.ForeColor = System.Drawing.Color.Green;
+                hdfEstado.Value = "0";
+                btnNuevo.Visible = true;
+                btnNuevo.Text = "Nuevo";
+                pnlTipoStorage.Visible = true;
+                pnlFormulario.Visible = false;
+                llenarGdvTiposStorage();
+            }
+        }//Fin de boton Guardar
+
+        protected void gdvTiposStorage_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                foreach (DataControlFieldCell cell in e.Row.Cells)
+                {
+                    foreach (Control control in cell.Controls)
+                    {
+                        //LinkButton button = (LinkButton)control;
+                        LinkButton button = control as LinkButton;
+                        if (button != null && button.Text == "Eliminar")
+                        {
+                            button.Enabled = permisos.D;
+                            if (button.Enabled)
+                                button.OnClientClick = "return checkMe()";
+                        }
+                    }
+                }
+            }
+        }//Fin de Row Data Bound
+
+        protected void gdvTiposStorage_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            lblStatus.Text = string.Empty;
+            int IdTipoStorage = Convert.ToInt32(gdvTiposStorage.Rows[e.RowIndex].Cells[1].Text);
+            Entidades.Logica.Ejecucion resultado = new Entidades.Logica.Ejecucion();
+            resultado = Negocio.Catalogos.TipoStorage.Eliminar(new Entidades.TipoStorage()
+            {
+                IdTipoStorage = IdTipoStorage
+            });
+
+            resultado.errores.ForEach(delegate (Entidades.Logica.Error error)
+            {
+                lblStatus.Text += error.descripcionCorta + "<br/>";
+            });
+
+            lblStatus.ForeColor = System.Drawing.Color.Red;
+            if (resultado.resultado == true)
+            {
+                lblStatus.ForeColor = System.Drawing.Color.Green;
+                llenarGdvTiposStorage();
+            }
+        }//Fin eliminar Fila
+
+    }//Fin de la Clase
+}
