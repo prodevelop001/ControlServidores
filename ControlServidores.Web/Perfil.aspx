997d0001d35169bcd0691e9d9643b99f18273955
Mauro A. <ma_10fig@hotmail.com>
2015-10-23 13:43:27 -0500

    Creando pagina de Perfil

---
+++ ControlServidores.Web/Perfil.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Sitio.Master" AutoEventWireup="true" CodeBehind="Perfil.aspx.cs" Inherits="ControlServidores.Web.Perfil" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="cabecera" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cuerpoPpal" runat="server">
+<div  class="principal">
+<div class="ttlPrincipal">
+    <h1>Perfil</h1>
+</div>
+    <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>
+            <div class="">
+                <h4>Usuario: </h4>
+                <asp:Label ID="lblUsrName" runat="server" Text=""></asp:Label>
+                <h4>Nombre: </h4>
+                <asp:Label ID="lblNombre" runat="server" Text=""></asp:Label>
+            </div>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+</div>
+</asp:Content>
---
+++ ControlServidores.Web/Perfil.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ControlServidores.Web
+{
+    public partial class Perfil : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Entidades.Usuarios usuario = (Entidades.Usuarios)Session["usuario"];
+            lblUsrName.Text = usuario.Usuario;
+
+            //int idPersona = Convert.ToInt32(usuario.IdUsuario);
+            int idPersona = Convert.ToInt32(usuario.IdPersona);
+
+            //List<Entidades.Personas> persona = Negocio.Seguridad.Personas.Obtener(new Entidades.Personas()
+            //{
+            //    IdPersona = 1
+            //}
+            //);
+
+            //Entidades.Personas p = persona.First();
+            //lblNombre.Text = p.Nombre;
+            lblNombre.Text = idPersona.ToString();
+        }
+    }
+}
